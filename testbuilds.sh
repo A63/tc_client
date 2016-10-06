5c14b0fe4c2b90e7b0325cd4e97a3e8d653f5adf
Alicia <alicia@ion.nu>
2016-10-07 00:11:32 +0200

    testbuilds.sh: adjusted for configure no longer accepting --enable-mic (since it's no longer experimental)

--- testbuilds.sh
+++ testbuilds.sh
@@ -5,7 +5,7 @@ echo
 echo "What it does is to configure and build the project with different features enabled and disabled to make sure that improvements didn't also break stuff in certain cases. This *will* erase anything you've already compiled."
 sleep 8
 make clean > /dev/null 2> /dev/null
-printf "Without mic, with gtk+-2.x, with streaming, without RTMP_DEBUG: "
+printf "Without mic support, with gtk+-2.x, without RTMP_DEBUG: "
 res="[31mbroken[0m"
 while true; do
   ./configure > /dev/null 2> /dev/null || break
@@ -15,7 +15,7 @@ while true; do
   if ! grep -q 'GTK_LIBS=.*-lgtk-[^- ]*-2' config.mk; then res="gtk+-2.x not found, can't test"; break; fi
   if ! grep -q '^AVCODEC_LIBS' config.mk; then res="libavcodec not found, can't test"; break; fi
   if ! grep -q '^SWSCALE_LIBS' config.mk; then res="libswscale not found, can't test"; break; fi
-  if ! grep -q '^LIBV4L2_LIBS' config.mk; then res="libv4l2 not found, can't test"; break; fi
+  sed -i -e '/^AO_LIBS/d' config.mk
   echo 'CFLAGS+=-Werror' >> config.mk
   if ! make utils > /dev/null 2> /dev/null; then
     sed -i -e 's/-Werror[^ ]*//g' config.mk
@@ -31,16 +31,15 @@ mv camviewer camviewer.gtk2 > /dev/null 2> /dev/null
 mv tc_client-gtk tc_client-gtk.gtk2 > /dev/null 2> /dev/null
 
 make clean > /dev/null 2> /dev/null
-printf "With mic, with gtk+-3.x, without streaming, with RTMP_DEBUG: "
+printf "With mic support, with gtk+-3.x, with RTMP_DEBUG: "
 res="[31mbroken[0m"
 while true; do
-  ./configure --enable-mic > /dev/null 2> /dev/null || break
+  ./configure > /dev/null 2> /dev/null || break
   if ! grep -q 'GTK_LIBS=.*-lgtk-3' config.mk; then res="gtk+-3.x not found, can't test"; break; fi
   if ! grep -q '^AO_LIBS' config.mk; then res="libao not found, can't test"; break; fi
   if ! grep -q 'RESAMPLE_LIBS' config.mk; then res="lib(av|sw)resample not found, can't test"; break; fi
   if ! grep -q '^AVCODEC_LIBS' config.mk; then res="libavcodec not found, can't test"; break; fi
   if ! grep -q '^SWSCALE_LIBS' config.mk; then res="libswscale not found, can't test"; break; fi
-  sed -i -e '/^LIBV4L2_LIBS/d' config.mk
   echo 'CFLAGS+=-DRTMP_DEBUG=1 -Werror' >> config.mk
   if ! make utils > /dev/null 2> /dev/null; then
     sed -i -e 's/-Werror[^ ]*//g' config.mk
